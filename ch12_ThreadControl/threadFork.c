dbcc1803ef0acdd766df97a5b116e6cbbf097b70
Daghlny <daghlny@gmail.com>
2017-12-29 21:25:23 +0800

    add some ch12 notes

---
+++ ch12_ThreadControl/threadFork.c
@@ -0,0 +1,46 @@
+
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+void*
+thr_fn(void *arg)
+{
+    printf("enter thr_fn function\n");
+    int var = 42;
+    pid_t pid = 0;
+
+    printf("Process: %d | Thread: %ld\n", getpid(), pthread_self());
+    fflush(stdout);
+
+    if ((pid = fork()) < 0) {
+        printf("fork in thread %ld has error\n", pthread_self());
+        exit(0);
+    } 
+    if (pid == 0) {
+        printf("Process: %d | Thread: %ld ", getpid(), pthread_self());
+        printf("var: %d\n", var);
+        exit(0);
+    } else {
+        waitpid(pid, NULL, 0);
+    }
+    return NULL;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pid_t pid;
+    printf("Process: %d | Thread: %ld\n", getpid(), pthread_self());
+
+    if (pthread_create(&tid, NULL, thr_fn, NULL) != 0) {
+        printf("pthread_create error\n");
+        exit(0);
+    }
+
+    exit(0);
+}
+
--- note_ch12_ThreadControl.cpp
+++ note_ch12_ThreadControl.cpp
@@ -17,3 +17,34 @@ int pthread_attr_getdetachstate(const pthread_attr_t *restrict attr,
                                 int *detachstate);
 int pthread_attr_setdetachstate(pthread_attr_t *attr, 
                                 int *detachstate);
+
+#include <pthread.h>
+int pthread_key_create(pthread_key_t *keyp, void (*destructor)(void *));
+int pthread_key_delete(pthread_key_t *key);
+int pthread_once(pthread_once_t *initflag, void (*initfn)(void));
+
+#include <pthread.h>
+/* return Thread Specific Data or NULL when not associated */
+void *pthread_getspecific(pthread_key_t key);
+int pthread_setspecific(pthread_key_t key, const void *value); // return 0 if success or error number when fail
+
+
+#include <pthread.h>
+// \state can be PTHREAD_CANCEL_ENABLE or PTHREAD_CANCEL_DISABLE
+int pthread_setcancelstate(int state, int *oldstate);
+void pthread_testcancel(void);
+// \type can be PTHREAD_CANCEL_DEFERRED or PTHREAD_CANCEL_ASYNCHRONOUS
+int pthread_setcanceltype(int type, int *oldtype);
+
+#include <signal.h>
+// \how can be SIG_BLOCK or SIG_SETMASK or SIG_UNBLOCK
+// if \set is NULL then \pthread_sigmask can get the old signal mask
+int pthread_sigmask(int how, const sigset_t *restrict set, sigset_t *restrict oset);
+// \signop is the amount of signals delivered
+int sigwait(const sigset_t *restrict set, int *restrict signop);
+int pthread_kill(pthread_t thread, int signo);
+
+#include <pthread.h>
+int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void));
+
+
